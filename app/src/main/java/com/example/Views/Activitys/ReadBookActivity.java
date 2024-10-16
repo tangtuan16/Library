f0424a8586aa1806fa4a051773ef9abffe0370e9
Duong-Duc-Khoi <150509827+Duong-Duc-Khoi@users.noreply.github.com>
2024-10-16 19:37:25 +0700

    page nhuw c ai nhu cac dau dau qua nghi thoi

--- app/src/main/java/com/example/Views/Activitys/ReadBookActivity.java
+++ app/src/main/java/com/example/Views/Activitys/ReadBookActivity.java
@@ -1,14 +1,19 @@
 package com.example.Views.Activitys;
 
 import android.os.Bundle;
-import android.widget.SeekBar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.Views.Adapters.BookContentAdapter;
-import com.example.Views.yCustom.TickSeekBar;
 import com.example.btl_libary.R;
 
 import org.json.JSONArray;
@@ -21,21 +26,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadBookActivity extends AppCompatActivity {
-
     private ViewPager2 viewPager2;
     private BookContentAdapter adapter;
-    TextView bookname;
-    TickSeekBar seekBarTextSize;
-    private float defaultTextSize;
+    private TextView bookname;
+    private Button btnIncreaseFontSize, btnDecreaseFontSize;
+    private float currentTextSize;
+    private static final float MIN_TEXT_SIZE = 39f;
+    private static final float MAX_TEXT_SIZE = 81f;
+    private static final float TEXT_SIZE_STEP = 5f;
+    private String bookContent;
+    List<String> textChunks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_book);
+
         bookname = findViewById(R.id.bookname);
         viewPager2 = findViewById(R.id.viewpager);
+        btnIncreaseFontSize = findViewById(R.id.btnIncreaseFontSize);
+        btnDecreaseFontSize = findViewById(R.id.btnDecreaseFontSize);
 
+        bookname.setText(getIntent().getStringExtra("title") + " - " + getIntent().getStringExtra("author"));
 
-        bookname.setText(getIntent().getStringExtra("title")+" - "+getIntent().getStringExtra("author"));
+        currentTextSize = getResources().getDimension(R.dimen.text_size);
+
         int bookId = getIntent().getIntExtra("bookId", -1);
         if (bookId != -1) {
             String jsonData = loadJSONFromAsset();
@@ -44,10 +58,17 @@ public class ReadBookActivity extends AppCompatActivity {
                 for (int i = 0; i < booksArray.length(); i++) {
                     JSONObject book = booksArray.getJSONObject(i);
                     if (book.getInt("id") == bookId) {
-                        String content = book.getString("content");
-                        List<String> textChunks = splitTextIntoChunks(content, 600);
-                        adapter = new BookContentAdapter(textChunks);
-                        viewPager2.setAdapter(adapter);
+                        bookContent = book.getString("content");
+                        textChunks = new ArrayList<>();
+                        textChunks.add(bookContent);
+                        int chapterNumber = 1;
+                        while (book.has("chuong" + chapterNumber)) {
+                            bookContent = book.getString("chuong" + chapterNumber);
+                            textChunks.add(bookContent);
+                            chapterNumber++;
+                        }
+
+                        updateContent();
                         break;
                     }
                 }
@@ -55,39 +76,86 @@ public class ReadBookActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-        seekBarTextSize = findViewById(R.id.seekBarTextSize);
 
-        // Lưu kích thước chữ mặc định
-        defaultTextSize = getResources().getDimension(R.dimen.text_size);
-
-        // Thiết lập SeekBar
-        seekBarTextSize.setMax(30); // Giả sử range từ 0 đến 30
-        seekBarTextSize.setProgress(15); // Giá trị mặc định
-
-        seekBarTextSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        btnIncreaseFontSize.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float scaleFactor = (progress + 10) / 25f; // Để tránh kích thước quá nhỏ
-                updateTextSize(defaultTextSize * scaleFactor);
+            public void onClick(View v) {
+                Log.d("ad", "Increase button clicked. Current size: " + currentTextSize);
+                changeTextSize(true);
             }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-
+        btnDecreaseFontSize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextSize(false);
+            }
+        });
     }
 
 
-    private void updateTextSize(float newSize) {
-        if (adapter != null) {
-            adapter.updateTextSize(newSize);
+
+    private void updateContent() {
+        adapter = new BookContentAdapter(textChunks, currentTextSize);
+        viewPager2.setAdapter(adapter);
+        viewPager2.setPageTransformer(new BookPageTransformer());
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        // Thêm padding cho ViewPager2 để hiệu ứng được nhìn thấy rõ hơn
+        int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, getResources().getDisplayMetrics());
+        viewPager2.setPadding(padding, 0, padding, 0);
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+
+    }
+
+    public class BookPageTransformer implements ViewPager2.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        @Override
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0f);
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1f);
+                view.setTranslationX(0f);
+                view.setTranslationZ(0f);
+                view.setScaleX(1f);
+                view.setScaleY(1f);
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+                // Move it behind the left page
+                view.setTranslationZ(-1f);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0f);
+            }
         }
     }
-    // Hàm để đọc file JSON từ thư mục assets
+    private void changeTextSize(boolean increase) {
+        if (increase && currentTextSize < MAX_TEXT_SIZE) {
+            currentTextSize += TEXT_SIZE_STEP;
+
+        } else if (!increase && currentTextSize > MIN_TEXT_SIZE) {
+            currentTextSize -= TEXT_SIZE_STEP;
+        }
+        updateContent();
+    }
+
+
     private String loadJSONFromAsset() {
         String json = null;
         try {
@@ -97,7 +165,6 @@ public class ReadBookActivity extends AppCompatActivity {
             is.read(buffer);
             is.close();
             json = new String(buffer, "UTF-8");
-            // Thay thế \n\n bằng \n\n\t để lùi dòng
             json = json.replace("\\n\\n", "\\n\\n\t\t\t\t");
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -106,12 +173,5 @@ public class ReadBookActivity extends AppCompatActivity {
         return json;
     }
 
-    private List<String> splitTextIntoChunks(String text, int chunkSize) {
-        List<String> chunks = new ArrayList<>();
-        int length = text.length();
-        for (int i = 0; i < length; i += chunkSize) {
-            chunks.add(text.substring(i, Math.min(length, i + chunkSize)));
-        }
-        return chunks;
-    }
+
 }
